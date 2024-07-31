99b7e1d69d4389590d102144207671c5fa3fd303
L-salamanca <luisferperezs@hotmail.com>
2024-07-31 11:26:10 -0500

    feat(Menu): :sparkles: Menu feature
    The template of the menu was created succesfully, as you can see the user interface counts with 5 options in spanish, they include registrar ciudad, ver ciudades, buscar ciudad, editar ciudad and salir, you can also select an option with the variable opc and depending of the choice a message will be displayed taking on count the function which is suppossed to present except for salir which leads you to the end of the program
    
    @L-salamanca

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+while True:
+    print("Bienvenido")
+    print("")
+    print("1.Registrar ciudad")
+    print("2.Ver ciudades")
+    print("3.Buscar ciudad")
+    print("4.Editar ciudad")
+    print("5.Salir")
+    opc=int(input("Elija una opcion: "))
+    
+    if opc == 1:
+        print("registrar ciudad (proceso)")
+    elif opc == 2:
+        print("ver ciudades  (proceso)")
+    elif opc == 3:
+        print("editar ciudad (proceso)")
+    elif opc == 4:
+        print("buscar ciudad (proceso)")
+    elif opc == 5:
+        print("")
+        print("Hasta pronto")
+        break
+
+
+
+
+
+
+

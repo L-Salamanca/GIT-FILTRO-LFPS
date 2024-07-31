540f79a3e84a3965c119719ce1683b3d8b547363
L-salamanca <luisferperezs@hotmail.com>
2024-07-31 12:16:15 -0500

    feat: :sparkles: Main function
    The main part of the project became a function to improve the software requirements to work
    
    @L-salamanca

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 
-
-while True:
-    print("Bienvenido")
-    print("")
-    print("1.Registrar ciudad")
-    print("2.Ver ciudades")
-    print("3.Buscar ciudad")
-    print("4.Eliminar ciudad")
-    print("5.Salir")
-    opc=int(input("Elija una opcion: "))
-    
-    if opc == 1:
-        nombre = input("Nombre: ")
-        codigo = input("Codigo postal: ")
-        poblacion = input("Poblacion: ")
-        pais = input("Pais: ")
-        registrar_ciudad(nombre, codigo, poblacion, pais)
-    elif opc == 2:
-        roles = listar_ciudad()
-        for ciudad in ciudades:
-            print(ciudad)
-    elif opc == 3:
-        print("buscar ciudad (proceso)")
-    elif opc == 4:
-        nombre = input("Ciudad a eliminar: ")
-        eliminar_ciudad(nombre)
-    elif opc == 5:
+def main():
+    while True:
+        print("Bienvenido")
         print("")
-        print("Hasta pronto")
-        break
+        print("1.Registrar ciudad")
+        print("2.Ver ciudades")
+        print("3.Buscar ciudad")
+        print("4.Eliminar ciudad")
+        print("5.Salir")
+        opc=int(input("Elija una opcion: "))
+        
+        if opc == 1:
+            nombre = input("Nombre: ")
+            codigo = input("Codigo postal: ")
+            poblacion = input("Poblacion: ")
+            pais = input("Pais: ")
+            registrar_ciudad(nombre, codigo, poblacion, pais)
+        elif opc == 2:
+            roles = listar_ciudad()
+            for ciudad in ciudades:
+                print(ciudad)
+        elif opc == 3:
+            print("buscar ciudad (proceso)")
+        elif opc == 4:
+            nombre = input("Ciudad a eliminar: ")
+            eliminar_ciudad(nombre)
+        elif opc == 5:
+            print("")
+            print("Hasta pronto")
+            break
+        else:
+                print("Opción no válida")
+
+if __name__ == "__main__":
+    main()
+
     
 
 
